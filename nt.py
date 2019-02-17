319763eb777fd15c3e9c25d979fc466cc646a046
ErikBoesen <me@erikboesen.com>
2019-02-16 22:20:10 -0500

    Remove class layout

--- nt.py
+++ nt.py
@@ -4,18 +4,6 @@ from networktables import NetworkTables
 from networktables.util import ntproperty
 import time
 
-TEAM = 1418
-
-class Interface:
-    def __init__(self):
-        NetworkTables.initialize(server='roborio-%d-frc.local')
-        self.value = NetworkTables.getGlobalAutoUpdateValue('/vision/targets_seen', 0, writeDefault=False)
-
-    def log(self):
-        print(self.value.value)
-
-if __name__ == '__main__':
-    interface = Interface()
-    while True:
-        interface.log()
-        time.sleep(0.5)
+NetworkTables.initialize(server='10.14.18.2')
+table = NetworkTables.getTable()
+print(table.getBoolean('robot/manual_lift_control', False))
