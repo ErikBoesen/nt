a072f7ed264c05fbece9bfb652431734e0e76b67
ErikBoesen <me@erikboesen.com>
2019-02-18 11:52:36 -0500

    Add refresh rate argument

--- nt.py
+++ nt.py
@@ -5,18 +5,20 @@ import socket
 from termcolor import colored
 import argparse
 
+"""
+def calculate_robot_ip():
+    my_ip = socket.gethostbyname(socket.getfqdn())
+    ip_bytes = my_ip.split('.')
+    ip_bytes[-1] = 2
+    rio_ip = '.'.join(ip_bytes)
+"""
+
 parser = argparse.ArgumentParser(description='Interact with NetworkTables')
 parser.add_argument('--robot', nargs=1, default='localhost')
+parser.add_argument('--refresh-rate', nargs=1, default=0.5)
+args = parser.parse_args()
 
-REFRESH_RATE = 0.5  # Hertz
-
-"""
-my_ip = socket.gethostbyname(socket.getfqdn())
-ip_bytes = my_ip.split('.')
-ip_bytes[-1] = 2
-rio_ip = '.'.join(ip_bytes)
-"""
-NetworkTables.initialize(server=rio_ip)
+NetworkTables.initialize(server=args.robot)
 
 def log(key, value, isNew):
     print('{key} => {value}'.format(key=colored(key, 'cyan'),
@@ -24,4 +26,4 @@ def log(key, value, isNew):
 
 NetworkTables.addEntryListener(log)
 while True:
-    time.sleep(1/REFRESH_RATE)
+    time.sleep(1/args.refresh_rate)
